79880ab2e3db86d17185d4e2c0632dfd9c74b1f1
jeremiahredekop <jeremiah@geniuscode.net>
2010-10-24 22:25:24 -0700

    added

---
+++ CommandingWithMvvm/ViewModels/Binding1ViewModel.cs
@@ -0,0 +1,82 @@
+﻿using GalaSoft.MvvmLight;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using System;
+
+namespace CommandingWithMvvm.ViewModels
+{
+
+    public class Binding1ViewModel : ViewModelBase
+    {
+
+        public Binding1ViewModel()
+        {
+            InitializeCommands();
+
+            if (IsInDesignMode)
+                Count = 3;
+            
+            
+        }
+
+        private void InitializeCommands()
+        {
+            // Action that command will perform
+            Action incrementAction = () => PerformIncrementCount();
+            // Predicate for whether or not action is allowed to execute
+            Func<bool> incrementPredicate = () => Count >= 0;
+
+            // Set Command property to new RelayCommand Object
+            IncrementCount = new RelayCommand(incrementAction,incrementPredicate);
+        }
+
+        public void PerformIncrementCount()
+        {
+            Count++;
+        }
+
+        #region Count Property (INotify Property Changed)
+        
+        /// <summary>
+        /// The <see cref="Count" /> property's name.
+        /// </summary>
+        public const string CountPropertyName = "Count";
+
+        private int _Count = 0;
+
+        /// <summary>
+        /// Gets the Count property.
+        /// TODO Update documentation:
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// This property's value is broadcasted by the Messenger's default instance when it changes.
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return _Count;
+            }
+
+            set
+            {
+                if (_Count == value)
+                {
+                    return;
+                }
+
+                var oldValue = _Count;
+                _Count = value;
+
+                // Tell the UI to refresh its predicate
+                IncrementCount.RaiseCanExecuteChanged();
+                // Tell the UI to refresh databinding of Count Property
+                RaisePropertyChanged(CountPropertyName);
+
+            }
+        }
+        #endregion
+
+        public RelayCommand IncrementCount { get; set; }
+
+    }
+}
---
+++ CommandingWithMvvm/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,98 @@
+﻿
+
+namespace CommandingWithMvvm.ViewModels
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// <para>
+    /// Use the <strong>mvvmlocatorproperty</strong> snippet to add ViewModels
+    /// to this locator.
+    /// </para>
+    /// <para>
+    /// In Silverlight and WPF, place the ViewModelLocator in the App.xaml resources:
+    /// </para>
+    /// <code>
+    /// &lt;Application.Resources&gt;
+    ///     &lt;vm:ViewModelLocator xmlns:vm="clr-namespace:CommandingWithMvvm.ViewModels"
+    ///                                  x:Key="Locator" /&gt;
+    /// &lt;/Application.Resources&gt;
+    /// </code>
+    /// <para>
+    /// Then use:
+    /// </para>
+    /// <code>
+    /// DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
+    /// </code>
+    /// <para>
+    /// You can also use Blend to do all this with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm/getstarted
+    /// </para>
+    /// </summary>
+    public class ViewModelLocator
+    {
+
+        private static Binding1ViewModel _binding1ViewModel;
+
+        /// <summary>
+        /// Gets the Binding1ViewModel property.
+        /// </summary>
+        public static Binding1ViewModel Binding1ViewModelStatic
+        {
+            get
+            {
+                if (_binding1ViewModel == null)
+                {
+                    CreateBinding1ViewModel();
+                }
+
+                return _binding1ViewModel;
+            }
+        }
+
+        /// <summary>
+        /// Gets the Binding1ViewModel property.
+        /// </summary>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance",
+            "CA1822:MarkMembersAsStatic",
+            Justification = "This non-static member is needed for data binding purposes.")]
+        public Binding1ViewModel Binding1ViewModel
+        {
+            get
+            {
+                return Binding1ViewModelStatic;
+            }
+        }
+
+        /// <summary>
+        /// Provides a deterministic way to delete the Binding1ViewModel property.
+        /// </summary>
+        public static void ClearBinding1ViewModel()
+        {
+            _binding1ViewModel.Cleanup();
+            _binding1ViewModel = null;
+        }
+
+        /// <summary>
+        /// Provides a deterministic way to create the Binding1ViewModel property.
+        /// </summary>
+        public static void CreateBinding1ViewModel()
+        {
+            if (_binding1ViewModel == null)
+            {
+                _binding1ViewModel = new Binding1ViewModel();
+            }
+        }
+
+        /// <summary>
+        /// Cleans up all the resources.
+        /// </summary>
+        public static void Cleanup()
+        {
+            ClearBinding1ViewModel();
+        }
+
+    }
+}
